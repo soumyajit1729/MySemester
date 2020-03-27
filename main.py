fb06ac3f1beee1767828936ea0178d7e99452f5e
soumyajit <soumyajit1729@gmail.com>
2020-03-27 21:45:51 +0530

    Initial commit: upto 11 min

---
+++ main.py
@@ -0,0 +1,24 @@
+class Data:
+	pass
+
+class Population:
+	pass
+
+class GeneticAlgo:
+	pass
+
+class Course:
+	pass
+
+class Instructor:
+	pass
+
+class Room:
+	pass
+
+class ClassName(object):
+	"""docstring for ClassName"""
+	def __init__(self, arg):
+		super(ClassName, self).__init__()
+		self.arg = arg
+		
